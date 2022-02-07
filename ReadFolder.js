b122e4bf21ba2d5929d42373716416ff9fd7ad3f
PhongNgo1776 <ngothanhphong1776@gmail.com>
2022-02-08 01:08:31 +0700

    hh

--- ReadFolder.js
+++ ReadFolder.js
@@ -1,9 +1,9 @@
-const testFolder = './SHRIMPS_LOBSTER/Shrimp';
+const testFolder = './Mantis/IMG';
 const fs = require('fs');
 
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
-    fs.appendFile('data.txt', '- assets/images/thumb_styles/Shrimp/' + file + '\n', function (err) {
+    fs.appendFile('data.txt', "\"" + file + '\",\n', function (err) {
       if (err) return console.log(err);
     });
   });
