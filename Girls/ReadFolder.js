14c1a5c115374be3a1bbe0abe01236b6a3e31433
PhongNgo1776 <ngothanhphong1776@gmail.com>
2022-02-09 22:13:09 +0700

    d

---
+++ Girls/ReadFolder.js
@@ -0,0 +1,10 @@
+const testFolder = './IMG/StandingStyle';
+const fs = require('fs');
+
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    fs.appendFile('data.txt', "\"" + file + '\",\n', function (err) {
+      if (err) return console.log(err);
+    });
+  });
+});
