e2901e41ce987b54132cc1b087304a1899e74d3b
PhongNgo1776 <ngothanhphong1776@gmail.com>
2022-02-12 01:02:34 +0700

    s

--- Girls/ReadFolder.js
+++ Girls/ReadFolder.js
@@ -1,8 +1,11 @@
-const testFolder = './IMG/StandingStyle';
+const testFolder = './Test/';
 const fs = require('fs');
+const sharp = require('sharp');
+
 
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
+    sharp(testFolder + file).resize({ width: 100 }).toFile(testFolder + '/thumbnals/' + file);
     fs.appendFile('data.txt', "\"" + file + '\",\n', function (err) {
       if (err) return console.log(err);
     });
