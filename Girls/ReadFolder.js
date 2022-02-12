1d36e2813361dbb580a203de6f4599653373feec
PhongNgo1776 <ngothanhphong1776@gmail.com>
2022-02-12 22:59:27 +0700

    dsd

--- Girls/ReadFolder.js
+++ Girls/ReadFolder.js
@@ -1,13 +1,18 @@
-const testFolder = './Test/';
+const rootFolder = './IMG/OutsideGarden/';
+const destFolder = 'thumbnail/OutsideGarden/';
 const fs = require('fs');
 const sharp = require('sharp');
 
 
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    sharp(testFolder + file).resize({ width: 100 }).toFile(testFolder + '/thumbnals/' + file);
-    fs.appendFile('data.txt', "\"" + file + '\",\n', function (err) {
-      if (err) return console.log(err);
-    });
+fs.readdir(rootFolder, (err, files) => {
+  files.forEach(filename => {
+    if(filename.includes(' (1)') || filename.includes(' (2)') || filename.includes(' (3)')){
+      fs.unlinkSync(rootFolder + filename);
+    }else{
+      sharp(rootFolder + filename).resize({ width: 100 }).toFile(destFolder + filename);
+      fs.appendFile('data.txt', "\"" + filename + '\",\n', function (err) {
+        if (err) return console.log(err);
+      });
+    }
   });
 });
