e6f9403da54974edaed609f3237ccd75a91a688f
PhongNgo <ptngo@akvagroup.com>
2022-02-27 20:30:33 +0700

    v

---
+++ HermitCrab/ReadFolder.js
@@ -0,0 +1,18 @@
+const rootFolder = './IMG/';
+const destFolder = 'thumbnail/';
+const fs = require('fs');
+const sharp = require('sharp');
+
+
+fs.readdir(rootFolder, (err, files) => {
+  files.forEach(filename => {
+    if(filename.includes(' (1)') || filename.includes(' (2)') || filename.includes(' (3)')){
+      fs.unlinkSync(rootFolder + filename);
+    }else{
+      sharp(rootFolder + filename).resize({ width: 100 }).toFile(destFolder + filename);
+      fs.appendFile('data.txt', "\"" + filename + '\",\n', function (err) {
+        if (err) return console.log(err);
+      });
+    }
+  });
+});
