f37cd5c48104743f03382df7c1000f1ad5e67681
PhongNgo <ptngo@akvagroup.com>
2022-04-18 22:55:38 +0700

    D

--- ReadFolder.js
+++ ReadFolder.js
@@ -1,5 +1,5 @@
-const rootFolder = './Wallpapers/IMG/';
-const destFolder = './Wallpapers/thumbnail/';
+const rootFolder = './Girls/NEW_test/';
+const destFolder = './Girls/thumbnail_test/';
 const fs = require('fs');
 const sharp = require('sharp');
 
@@ -9,6 +9,13 @@ fs.readdir(rootFolder, (err, files) => {
     if (filename.includes('.DS_Store') || filename.includes(' (1)') || filename.includes(' (2)') || filename.includes(' (3)')){
       fs.unlinkSync(rootFolder + filename);
     }else{
+      if (filename.includes('.jpeg')){
+        var newFileName = filename.split('.')[0] + '.jpg';
+        fs.rename(rootFolder + filename, rootFolder + newFileName, function (err) {
+          if (err) console.log('ERROR: ' + err);
+        });
+      }
+      
       sharp(rootFolder + filename).resize({ width: 100 }).toFile(destFolder + filename);
       fs.appendFile('data.txt', "\"" + filename + '\",\n', function (err) {
         if (err) return console.log(err);
